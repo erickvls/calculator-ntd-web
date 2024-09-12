2cb7fbc57d5e456449eef9229b0e3cbbc247831e
Erick Sousa <erick.v.l.s@gmail.com>
2024-09-12 18:16:59 -0400

    refactor: Records table - removed id column

--- src/utils/getColumns.tsx
+++ src/utils/getColumns.tsx
@@ -3,7 +3,6 @@ import { IconButton } from '@mui/material';
 import DeleteIcon from '@mui/icons-material/Delete';
 
 export const getColumns = (handleOpenDialog: (id: string) => void): GridColDef[] => [
-    { field: 'id', headerName: 'ID', flex: 1 },
     { field: 'userBalance', headerName: 'User Balance', flex: 1 },
     { field: 'operationResponse', headerName: 'Operation Response', flex: 1 },
     { field: 'date', headerName: 'Date', flex: 1 },
